d666688ae318862f37219bee31b239e605b8a37e
Osman122 <63487841+Osman122@users.noreply.github.com>
2023-12-13 18:07:32 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Todo from './Component/creatToDo';
+import Register from './Component/register';
+
+function App() {
+  return (
+    <div className="p-5 m-5">
+     <Todo />
+    </div>
+  );
+}
+
+export default App;
